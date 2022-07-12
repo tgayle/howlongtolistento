a847e526aff9e2ed9c0482c1fe8e024126a93b87
tgayle <travisgayledev@gmail.com>
2022-07-12 11:55:58 -0700

    loading indicator on search bar

--- app/components/SearchArtistBar.tsx
+++ app/components/SearchArtistBar.tsx
@@ -1,9 +1,10 @@
-import { useFetcher } from "@remix-run/react";
+import { useFetcher, useTransition } from "@remix-run/react";
 import { type SearchArtistResponse } from "~/spotify.server";
 import { ArtistItem } from "./ArtistItem";
 
 export function SearchArtistBar({ initialValue }: { initialValue?: string }) {
   const autocomplete = useFetcher<SearchArtistResponse>();
+  const transition = useTransition();
 
   return (
     <div className="flex flex-col w-full items-center justify-center">
@@ -16,16 +17,41 @@ export function SearchArtistBar({ initialValue }: { initialValue?: string }) {
         action="/artist-autocomplete"
         className="block relative"
       >
-        <input
-          placeholder="Drake?"
-          className="h-12 w-full sm:w-80 lg:w-96 p-4 rounded-lg my-4 shadow-lg block"
-          name="artist"
-          autoComplete="off"
-          defaultValue={initialValue}
-          onChange={(e) => {
-            autocomplete.submit(e.target.form);
-          }}
-        />
+        <div className="relative my-4">
+          <input
+            placeholder="Drake?"
+            className="h-12 w-full sm:w-80 lg:w-96 p-4 rounded-lg shadow-lg block"
+            name="artist"
+            autoComplete="off"
+            defaultValue={initialValue}
+            onChange={(e) => {
+              autocomplete.submit(e.target.form);
+            }}
+          />
+
+          <svg
+            className={`animate-spin text-blue-800 h-1/2 absolute right-0 top-0 mr-3 mt-3 ${
+              transition.state === "loading" ? "opacity-100" : "opacity-0"
+            }`}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
 
         <ul className="absolute w-full rounded-lg bg-gray-800">
           {autocomplete.data?.map((it) => (
